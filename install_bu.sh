3902e292900a86548c6a7a11813f378923bbefa8
jjz <woaf1003@gmail.com>
2017-03-23 11:54:31 +0800

    add install_bu

---
+++ install_bu.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if  which bitcoin-cli 2>/dev/null; then
+    echo 'bitcoin exist'
+    bitcoin-cli backwallet /tmp/backp.dat
+    bitcoin-cli stop
+else
+    echo 'bitcoin does not exist'
+fi
+
+#sudo apt-get remove bitcoin*
+#sudo rm /etc/apt/source.list.d/bitcoin-*.*
+#sudo add-apt-repository ppa:bitcoin-unlimited/bu-ppa-nightly
+#sudo apt-get update
+#sudo apt-get install bitcoind
