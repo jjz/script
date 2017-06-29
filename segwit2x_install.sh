965d6287251bc91af669bd45c1952ee6bdaa2d35
jjz <woaf1003@gmail.com>
2017-06-29 08:58:37 +0800

    add segwit2x_install.sh

---
+++ segwit2x_install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -ex
+if which bitcoin-cli 2>/dev/null; then
+    echo 'bitcoin client exist'
+else
+    echo 'bitcoin clent does not exist'
+fi
+bitcoin_path="bitcoin-btc1"
+
+if [! -d "$bitcoin_path" ]; then
+    git clone git@github.com:btc1/bitcoin.git $bitcoin_path
+    sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+    sudo apt-get install libboost-all-dev
+    sudo apt-get install libdb4.8-dev libdb4.8++-dev
+    cd $bitcoin_path
+    git checkout segwit2x
+else
+    cd $bitcoin_path
+    git pull
+fi
+
+./autogen.sh
+./configure  --without-gui  --disable-wallet
+make && make check
+sudo make install
+
+bitcoind -testnet --daemon
